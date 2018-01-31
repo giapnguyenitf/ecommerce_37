e2755880233bd653c73c8c56afbedced0b9d897d
giapnguyenitf <giapnguyenitf@gmail.com>
2018-01-31 15:24:10 +0700

    + create static page

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@lang('label.u_stora')</title>
+
+    {{ Html::style('css/app.css') }}
+    {{ Html::style('css/style.css') }}
+    {{ Html::style('css/responsive.css') }}
+    {{ Html::style('css/owl.carousel.css') }}
+    @yield('css')
+</head>
+
+<body>
+    <div class="header-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="user-menu">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-user"></i> @lang('label.my_account')</a>
+                            </li>
+                            <li>
+                                <a href="cart.html">
+                                    <i class="fa fa-user"></i> @lang('label.my_cart')</a>
+                            </li>
+                            <li>
+                                <a href="checkout.html">
+                                    <i class="fa fa-user"></i> @lang('label.checkout')</a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fa fa-user"></i> @lang('label.login')</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="header-right">
+                        <ul class="list-unstyled list-inline">
+                            <li class="dropdown dropdown-small">
+                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#">
+                                    <span class="key">@lang('label.currency') :</span>
+                                    <span class="value">@lang('label.VND') </span>
+                                    <b class="caret"></b>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="#">@lang('label.USD')</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">@lang('label.GBP')</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li class="dropdown dropdown-small">
+                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#">
+                                    <span class="key">@lang('label.language') :</span>
+                                    <span class="value">@lang('label.english') </span>
+                                    <b class="caret"></b>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href="#">@lang('label.english')</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">@lang('label.vietnamese')</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="site-branding-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="logo">
+                        <h1>
+                            <a href="#">
+                                <img src="{{ URL::asset('images/logo.png') }}">
+                            </a>
+                        </h1>
+                    </div>
+                </div>
+
+                <div class="col-sm-6">
+                    <div class="shopping-item">
+                        <a href="">@lang('label.cart') -
+                            <span class="cart-amunt">#</span>
+                            <i class="fa fa-shopping-cart"></i>
+                            <span class="product-count">#</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End site branding area -->
+
+    <div class="mainmenu-area">
+        <div class="container">
+            <div class="row">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="">@lang('label.home')</a>
+                        </li>
+                        <li>
+                            <a href="#">@lang('label.categories')</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @yield('content')
+    
+    <div class="footer-top-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-about-us">
+                        <h2>@lang('label.u')
+                            <span>@lang('label.stora')</span>
+                        </h2>
+                        <div class="footer-social">
+                            <a href="#" target="_blank">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                            <a href="#" target="_blank">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                            <a href="#" target="_blank">
+                                <i class="fa fa-youtube"></i>
+                            </a>
+                            <a href="#" target="_blank">
+                                <i class="fa fa-linkedin"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-menu">
+                        <h2 class="footer-wid-title">@lang('label.user_navigation') </h2>
+                        <ul>
+                            <li>
+                                <a href="#">@lang('label.my_account')</a>
+                            </li>
+                            <li>
+                                <a href="#">@lang('label.order_history')</a>
+                            </li>
+                            <li>
+                                <a href="#">@lang('label.wishlist')</a>
+                            </li>
+                            <li>
+                                <a href="#">@lang('label.vendor_contact')</a>
+                            </li>
+                            <li>
+                                <a href="#">@lang('label.front_page')</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-menu">
+                        <h2 class="footer-wid-title">@lang('label.categories')</h2>
+                        <ul>
+                            <li>
+                                <a href="#">@lang('label.mobile_phone')</a>
+                            </li>
+                            <li>
+                                <a href="#">@lang('label.home_accesseries')</a>
+                            </li>
+                            <li>
+                                <a href="#">@lang('label.led_tv')</a>
+                            </li>
+                            <li>
+                                <a href="#">@lang('label.computer')</a>
+                            </li>
+                            <li>
+                                <a href="#">@lang('label.gadets')</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-newsletter">
+                        <h2 class="footer-wid-title">@lang('label.newsletter')</h2>
+                        <p>@lang('label.news_letter_description')</p>
+                        <div class="newsletter-form">
+                            {{ Form::open(['url' => '/', 'method' => 'POST']) }}
+                                {{ Form::text('email', null, ['placeholder' => trans('label.type_your_email')]) }}
+                                {{ Form::submit(trans('label.subcribe')) }}
+                            {{ Form::close() }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="copyright">
+                        <p>&copy; @lang('label.copy_right')
+                            <a href="#" target="_blank"></a>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="footer-card-icon">
+                        <i class="fa fa-cc-discover"></i>
+                        <i class="fa fa-cc-mastercard"></i>
+                        <i class="fa fa-cc-paypal"></i>
+                        <i class="fa fa-cc-visa"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @yield('javascript')
+    {{ Html::script('js/app.js') }}
+    {{ Html::script('js/custom/owl.carousel.min.js') }}
+    {{ Html::script('js/custom/jquery.sticky.js') }}
+    {{ Html::script('js/custom/jquery.easing.1.3.min.js') }}
+    {{ Html::script('js/custom/main.js') }}
+    {{ Html::script('js/custom/bxslider.min.js') }}
+    {{ Html::script('js/custom/script.slider.js') }}
+</body>
+
+</html>
